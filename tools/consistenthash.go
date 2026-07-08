f602593621bac8e17f3a82478061d6194f0da172
apoi <cyq2290881657@outlook.com>
2026-07-08 18:24:35 +0800

    搬运了之前写的一致性哈希

---
+++ tools/consistenthash.go
@@ -0,0 +1,65 @@
+package tools
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+type Hash func(data []byte) uint32
+
+type Map struct {
+	hash     Hash
+	replicas int
+	keys     []int // Sorted
+	hashMap  map[int]string
+}
+
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+func (m *Map) Remove(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			hash := int(m.hash([]byte(strconv.Itoa(i) + key)))
+			idx := sort.SearchInts(m.keys, hash)
+			if idx < len(m.keys) && m.keys[idx] == hash {
+				m.keys = append(m.keys[:idx], m.keys[idx+1:]...)
+				delete(m.hashMap, hash)
+			} else {
+				// Key not found, handle as needed (e.g., log, ignore, etc.)
+			}
+		}
+	}
+}
+
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+	hash := int(m.hash([]byte(key)))
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	return m.hashMap[m.keys[idx%len(m.keys)]]
+}
